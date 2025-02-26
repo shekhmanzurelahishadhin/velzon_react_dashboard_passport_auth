fa8d4ce2cb1b4cc63e6081926838dec4efb67650
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-02-26 22:27:37 +0600

    Site setting

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -125,6 +125,7 @@ export default function Sidebar() {
                                         <li className="nav-item">
                                             <a href="#sidebarEmail" className="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarEmail" data-key="t-email">
                                                 Email
+                                                
                                             </a>
                                             <div className="collapse menu-dropdown" id="sidebarEmail">
                                                 <ul className="nav nav-sm flex-column">
@@ -134,7 +135,7 @@ export default function Sidebar() {
                                                     <li className="nav-item">
                                                         <a href="#sidebaremailTemplates" className="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebaremailTemplates" data-key="t-email-templates">
                                                             Email Templates
-                                                            
+
                                                         </a>
                                                         <div className="collapse menu-dropdown" id="sidebaremailTemplates">
                                                             <ul className="nav nav-sm flex-column">
